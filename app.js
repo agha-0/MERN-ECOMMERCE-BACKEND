5456263472287cf6fd248a48c33cbc3c05b7890f
Ahmer Bilal <86891147+Ahmer8856@users.noreply.github.com>
2023-04-17 14:56:42 +0500

    set database url

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
@@ -9,8 +10,8 @@ const errors = require ("./middleware/errors");
 const app = express();
 app.use(express.json({ limit: '100mb', }))
 app.use(express.urlencoded({ limit: '100mb', extended: true }))
-const connectDB = require('./database/DB.Connect')
-
+// const connectDB = require('./database/DB.Connect')
+const {MONGO_DB_CONFIG} = require("./config/app.config");
 
 const userRouter = require('./routes/userRouter')
 
@@ -26,17 +27,39 @@ app.use(errors.errorHandler);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(bodyParser.json())
+const dotenv = require("dotenv");
 
-const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port,
-            console.log("Listen at PORT ", port)
-        )
-    } catch (error) {
-        console.log(error);
+dotenv.config();
+
+mongoose.Promise = global.Promise;
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_DB_CONFIG.DB,{
+
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+
+} )
+.then(
+    ()=>{
+        console.log('database connected successfully to AUS_mobile_app');
+    },
+    (error)=>{
+        console.log('there is error while connecting DB'+error);
     }
-}
+);
 
+// const start = async () => {
+//     try {
+//         await connectDB(process.env.MONGO_URI);
+//         app.listen(port,
+//             console.log("Listen at PORT ", port)
+//         )
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
-start()
+// start()
+const server = app.listen(process.env.port || 5000, function () {
+    console.log("app is listening on the AUS COSMETICS");
+});
