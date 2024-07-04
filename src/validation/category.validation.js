a3c8c7ffeab00d36ef7e357aa446f1f13bfe5b41
agha-0 <ahmedtayyab533@gmail.com>
2024-07-04 12:42:29 +0500

    code pushed

--- src/validation/category.validation.js
+++ src/validation/category.validation.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 
 export const CategoryValidation = {
-  // add: {
-  //   body: Joi.object().keys({
-  //     name: Joi.string().required()
-  //   }),
-  // },
+  add: {
+    body: Joi.object().keys({
+      name: Joi.string().required()
+    }),
+  },
   id: {
     params: Joi.object().keys({
       id: Joi.string().required(),
